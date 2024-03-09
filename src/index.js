1abe9f85b39f47d1a72583b503f093a9f44bf5d5
Paupog <154589264+Paupog@users.noreply.github.com>
2024-03-09 12:46:30 +0100

    Api added
    it seems not to work

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-function  handleSearchSubmit(event) {
+
+
+function searchCity(city) {
+    let apiKey = "dfd0t6478f0367aa31fdf9o1c7ab1790";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    console.log(apiUrl); 
+}
+
+function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
 }
-
 
 let searchForElement = document.querySelector(".search-form");
 searchForElement.addEventListener("submit", handleSearchSubmit);
