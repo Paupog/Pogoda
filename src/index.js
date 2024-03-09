c3d4acf01d2528c4da74f6d0fb38271174d6c356
Paupog <154589264+Paupog@users.noreply.github.com>
2024-03-09 15:55:59 +0100

    Everything seems to work just fine with another function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-
+function refreshWeather(resposne) {
+    console.log(resposne.data);
+}
 
 function searchCity(city) {
     let apiKey = "dfd0t6478f0367aa31fdf9o1c7ab1790";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    console.log(apiUrl); 
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
@@ -11,7 +13,8 @@ function handleSearchSubmit(event) {
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
-let searchForElement = document.querySelector(".search-form");
+let searchForElement = document.querySelector("#search-form");
 searchForElement.addEventListener("submit", handleSearchSubmit);
